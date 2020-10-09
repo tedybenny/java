29a6acae75a72b5a2ec7bcd53b06e2ca7ad36d18
tedybenny <70962767+tedybenny@users.noreply.github.com>
2020-10-09 10:30:47 +0300

    Add files via upload

---
+++ Practica.java
@@ -0,0 +1,12 @@
+class Practica
+{
+	public static void main (String[] args) {
+
+		int x = 2;
+		int y = 2;
+		int lRebra = 5;
+		System.out.println((3+ Math.pow(Math.E,y-1)) / 1 + Math.pow(x,2) * Math.abs(y - Math.tan(x)));
+		System.out.println(Math.sin(Math.sqrt(x+1)) - Math.sin(Math.sqrt(x-1)));
+		System.out.println(6 * Math.pow(lRebra,2));
+	}
+}
